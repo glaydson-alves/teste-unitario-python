58e27afe741113e4430beca7fc7fb31067bd8942
Lucas Augusto <marquesribeirolucas@gmail.com>
2023-10-09 20:53:05 -0300

    iniciado teste python

--- tests/test_funcionario.py
+++ tests/test_funcionario.py
@@ -5,3 +5,11 @@ colaborador = Funcionario("Sofia","Gerente",3000)
 def test_verificarDados():
     assert colaborador.dados() == "Sofia seu cargo é Gerente e seu salário é 3000"
 
+def test_calcularBonus():
+    assert colaborador.calcularBonus() >= 300
+    # valor de calcularBonus será maior ou igual a 300.
+
+# python -m pytest test_funcionario.py < = executar esse comando no terminal
+
+def test_registrarPonto():
+    assert colaborador.registrarPonto() == "Ponto registrado na data 2023-10-09"
